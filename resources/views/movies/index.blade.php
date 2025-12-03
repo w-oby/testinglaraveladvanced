089dbe15f8a1078b843cf74f17668208344d750b
w-oby <w-oby>
2025-12-03 14:09:02 +0100

    Changed some text + better url pathing instead of hardcoded

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,12 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movies Index</title>
+    <style>
+        body {
+
+            font-family: Verdana, Geneva, Tahoma, sans-serif
+
+        }
+    </style>
 </head>
 <body>
     <h1>Movies overview</h1>
     <ul>
-        <li><a href="/movies/all_movies">All movies ordered by rating</a></li>
-        <li><a href="/movies/genre">Movies sorted by genres</a></li>
+        <li><a href="/movies/rating">Movies by rating</a></li>
+        <li><a href="/movies/genre">Movies by genres</a></li>
     </ul>
+
+<p>You could also search movies based on the url, such as <a href="{{ url('/movies/show/Lion') }}">{{ url('/movies/show/Lion') }}</a></p>
+
 </body>
 </html>
