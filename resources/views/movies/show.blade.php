97ed6073701c05b38a876836b3e0dcac0bac8b9b
w-oby <w-oby>
2025-12-03 10:16:50 +0100

    amended variables + added link

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -9,17 +9,22 @@
 <body>
     
     <h1>Movie details</h1>
+
     <ul>
 
         {{-- Display selected movie details (title, genre, release year, rating and description) --}}
-        @foreach ($allMovies as $movie)
-        
+        @foreach ($singleNovie as $movie)
+
         <li>{{ $movie->title }}</li>
         <li>{{ $movie->genre }}</li>
         <li>{{ $movie->release_year }}</li>
         <li>{{ $movie->rating }}</li>
         <li>{{ $movie->description }}</li>
+
     </ul>
+
+    <br><br>
+    <a href="/movies/index">Back to index</a>
 
 </body>
 </html>
