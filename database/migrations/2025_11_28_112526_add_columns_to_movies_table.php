0a1c544b5a96d297ce5fc6fd33429f14ed400b5f
w-oby <w-oby>
2025-12-03 09:45:24 +0100

    Made table and oclumns for movies

---
+++ database/migrations/2025_11_28_112526_add_columns_to_movies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->string('title');
+            $table->string('genre');
+            $table->integer('release_year');
+            $table->integer('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropColumn(['title', 'genre', 'release_year', 'rating']);
+        });
+    }
+};
