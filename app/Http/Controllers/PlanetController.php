528f5b082141f8cbfa74630e925c495c82d3db17
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2025-12-06 20:28:09 +0100

    Changed DB to Eloquent

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
+use App\Models\Planet;
 use Illuminate\View\View;
 
 class PlanetController extends Controller
@@ -11,15 +12,18 @@ class PlanetController extends Controller
     public function index(): View
     {
 
-        $planets = DB::table('planets')->get();
+        // $planets = DB::table('planets')->get();
+        $planets = Planet::all();
 
         return view('planets.index', ['planets' => $planets]);
+
     }
 
     public function show(string $name): View
     {
 
-        $planet = DB::table('planets')->where('name', $name)->first();
+        // $planet = DB::table('planets')->where('name', $name)->first();
+        $planet = Planet::where('name', $name)->firstOrFail();
 
         return view('planets.show', ['planet' => $planet]);
 
