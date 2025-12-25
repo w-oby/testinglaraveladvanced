e7a233aeab0664d68576f5aaa8280bc250d20a5a
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2025-12-25 01:06:48 +0100

    Added named routes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,16 @@
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanetController;
-
+use App\Http\Controllers\SolarSystemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // PLANETS
-Route::get('/planets', [PlanetController::class, 'index']);
+Route::get('/planets', [PlanetController::class, 'index'])->name('planets.index');
 
-Route::get('/planets/{name}', [PlanetController::class, 'show']);
+Route::get('/planets/{name}', [PlanetController::class, 'show'])->name('planets.show');
 
 // MOVIES
 Route::get('/movies', [MovieController::class, 'index']);
@@ -22,3 +22,8 @@ Route::get('/movies/rating', [MovieController::class, 'allMoviesOrderedByRating'
 Route::get('/movies/show/{title}', [MovieController::class, 'show']);
 
 Route::get('/movies/genre', [MovieController::class, 'showByGenre']);
+
+// SOLARSYSTEMS
+Route::get('/solarsystems', [SolarSystemController::class, 'index'])->name('solarsystems.index');
+
+Route::get('/solarsystems/{id}', [SolarSystemController::class, 'show'])->name('solarsystems.show');
